44f16b05d6ce9e28ee5462199e1c26eb6fbfe45e
jjspace <jjrouzer@yahoo.com>
2019-11-08 16:31:01 -0500

    feat(pm2): add pm2 ecosystem configuration

---
+++ pm2-ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [{
+    name: 'doodle-bot',
+    script: './src/bot.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    wait_ready: true,
+    listen_timeout: 10000,
+    max_restarts: 10,
+    max_memory_restart: '150M',
+    log_file: 'doodle-bot.log',
+    time: true,
+    merge_logs: true,
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
+  }],
+};
